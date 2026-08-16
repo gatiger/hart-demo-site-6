2a1894f8754af916ae3bd928fc6c53d2d8c9564a
gatiger <ivafireman@gmail.com>
2026-08-16 17:18:04 -0400

    ems protocols v126

--- ems-protocols/app.js
+++ ems-protocols/app.js
@@ -3,7 +3,7 @@ let deferredPrompt;
 let selectedId = null;
 let viewHistory = [];
 let viewHistoryIndex = -1;
-const CONTENT_VERSION = 'v125';
+const CONTENT_VERSION = 'v126';
 
 const treeNav = document.getElementById('treeNav');
 const searchBox = document.getElementById('searchBox');
--- ems-protocols/service-worker.js
+++ ems-protocols/service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'hcems-protocols-v125';
+﻿const CACHE_NAME = 'hcems-protocols-v126';
 const APP_SHELL = [
   './',
   './index.html',
